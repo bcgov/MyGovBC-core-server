f6c6257fb5fa1244ca5f112a3b42f621bcd167c2
f-w <fred.wen@gov.bc.ca>
2016-08-25 16:33:47 -0700

    protected and pruned service methods.

--- common/models/service.js
+++ common/models/service.js
@@ -1,3 +1,21 @@
-module.exports = function(Service) {
+module.exports = function (Service) {
+  Service.disableRemoteMethod('findOne', true)
+  Service.disableRemoteMethod('findById', true)
+  Service.disableRemoteMethod('createChangeStream', true)
+  Service.disableRemoteMethod('exists', true)
+  Service.disableRemoteMethod('updateAll', true)
+  Service.disableRemoteMethod('count', true)
+  Service.disableRemoteMethod('upsert', true)
 
-};
+  Service.beforeRemote('**', function () {
+    var ctx = arguments[0]
+    var next = arguments[arguments.length - 1]
+    var u = ctx.req.get('sm_user') || ctx.req.get('smgov_userdisplayname')
+    var error
+    if (u && ctx.method.accessType === 'WRITE') {
+      error = new Error('Unauthorized')
+      error.status = 401
+    }
+    next(error)
+  })
+}
